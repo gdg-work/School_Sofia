29af2d13f66c16271207bcbeb5dda0db3d507f02
Dmitriy Golub <gdg.work@gmail.com>
2017-04-23 13:58:20 +0500

    Added some students' certificates

--- Content/deploy.sh
+++ Content/deploy.sh
@@ -2,6 +2,7 @@
 lektor="/usr/bin/lektor"
 netlify="/usr/bin/netlify"
 
+rm -r ../_Build/*
 ${lektor} clean -O ../_Build
 ${lektor} build -O ../_Build
 ${netlify} deploy -s sofia-perm -p ../_Build -t $(cat ~/.netlify/access.token)

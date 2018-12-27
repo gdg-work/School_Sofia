efb259a3917cf4ab6829c58eb2f3b518cfdbe022
Dmitriy Golub <gdg.work@gmail.com>
2018-12-27 21:29:50 +0500

    NY-2018

--- Content/deploy.sh
+++ Content/deploy.sh
@@ -2,4 +2,5 @@
 lektor="/usr/bin/lektor"
 netlify="/usr/bin/netlify"
 ${lektor} build -O ../_Build
-${netlify} deploy -s sofia-perm -p ../_Build -t $(cat ~/.netlify/access.token)
+# ${netlify} deploy -s sofia-perm -p ../_Build -t $(cat ~/.netlify/access.token)
+# s3cmd put --recursive ../_Build/* s3://sofia/

0c3c11e57de9316004ac3e1d082298af0047c5ce
Dmitriy Golub <gdg.work@gmail.com>
2019-08-29 11:52:07 +0500

    1 sentyabrya news article added

--- Content/deploy.sh
+++ Content/deploy.sh
@@ -4,3 +4,4 @@ netlify="/usr/bin/netlify"
 ${lektor} build -O ../_Build
 # ${netlify} deploy -s sofia-perm -p ../_Build -t $(cat ~/.netlify/access.token)
 # s3cmd put --recursive ../_Build/* s3://sofia/
+s3cmd sync ../_Build/ s3://sofia/
